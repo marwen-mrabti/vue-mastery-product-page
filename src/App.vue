5f640ca03e125d74813dcd096ff46b503a2140f5
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-15 14:49:30 +0100

    l3: conditional rendring with v-if/v-else and v-show

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { ref } from 'vue'
 import socksGreenImg from '@/assets/images/socks_green.jpeg'
 
-const product = ref('socks')
+const product = ref('Socks')
 const productDescription = ref('this socks are amazing!')
 const greenSocksImg = ref(socksGreenImg)
+const inventory = ref(10)
+const onSale = ref(true)
 </script>
 
 <template>
@@ -17,6 +19,12 @@ const greenSocksImg = ref(socksGreenImg)
         <div class="product-info">
           <h1>{{ product }}</h1>
           <p>{{ productDescription }}</p>
+          <p v-if="inventory > 10" class="text-green-400">In Stock</p>
+          <p v-else-if="inventory <= 10" class="text-orange-400">Almost Out of Stock</p>
+          <p v-else class="text-red-400">Out of Stock</p>
+          <small v-show="onSale" class="text-sm text-blue-400 font-semibold capitalize">
+            On Sale
+          </small>
         </div>
       </div>
     </div>
