9af2468d0a8714c38ecbef5639b2f01663042470
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-15 13:33:51 +0100

    l1: manage vue reactive state with ref

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const product = ref('socks')
+const productDescription = ref('this socks are amazing!')
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="max-w-screen min-h-screen flex justify-center items-start bg-amber-100">
+    <div class="flex flex-col gap-2">
+      <h1 class="text-3xl">{{ product }}</h1>
+      <p class="text-base">
+        {{ productDescription }}
+      </p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
