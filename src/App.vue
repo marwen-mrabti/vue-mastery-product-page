8c8c7a0b52a4f0b4114306a99ba1c9e8be5c1adb
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-15 22:37:14 +0100

    l7: computed properties using

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,49 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import GreenSocksImg from '@/assets/images/socks_green.jpeg'
 import BlueSocksImg from '@/assets/images/socks_blue.jpeg'
+import type { TVariant } from '@/types'
 
 const cart = ref<string[]>([])
 
 const product = ref('Socks')
+const brand = ref('Vue Mastery')
+
+const title = computed(() => {
+  return brand.value + ' ' + product.value
+})
+
 const productDescription = ref('this socks are amazing!')
 const details = ref(['50% coton', '30% wool', '20% polyester'])
 
-const variants = ref([
-  { id: 2234, color: 'green', imageUrl: GreenSocksImg },
-  { id: 2235, color: 'blue', imageUrl: BlueSocksImg },
+const variants = ref<TVariant[]>([
+  { id: 2234, color: 'green', imageUrl: GreenSocksImg, quantity: 0 },
+  { id: 2235, color: 'blue', imageUrl: BlueSocksImg, quantity: 50 },
 ])
+const selectedVariantIndex = ref<number>(0)
 
-const productImg = ref(GreenSocksImg)
-const inventory = ref(10)
+const productImg = computed(() => {
+  return selectedVariantIndex.value !== -1
+    ? variants.value[selectedVariantIndex.value].imageUrl
+    : ''
+})
+const inStock = computed(() => {
+  return selectedVariantIndex.value !== -1 && !!variants.value[selectedVariantIndex.value].quantity
+})
 const onSale = ref(true)
 
 const addToCart = () => {
   cart.value.push(product.value)
 }
 
-const setProductImage = (variantImg: string) => {
-  productImg.value = variantImg
+const updateVariant = (variantIndex: number) => {
+  selectedVariantIndex.value = variantIndex
 }
 </script>
 
 <template>
-  <main class="bg-amber-100 max-w-screen min-h-screen">
+  <div class="nav-bar"></div>
+  <main class="max-w-screen overflow-x-hidden">
     <div class="cart">Cart ({{ cart.length }})</div>
     <div class="product-display">
       <div class="product-container">
@@ -37,11 +52,11 @@ const setProductImage = (variantImg: string) => {
             :src="productImg"
             alt="socks with vue mastery logo"
             class="transition-all duration-200 ease-linear"
-            :class="!inventory && 'out-of-stock-img'"
+            :class="!inStock && 'out-of-stock-img'"
           />
         </div>
         <div class="product-info">
-          <h1>{{ product }}</h1>
+          <h1>{{ title }}</h1>
           <p>{{ productDescription }}</p>
           <ul class="pl-6 max-w-fit">
             <li v-for="(detail, index) in details" :key="index" class="capitalize">
@@ -50,9 +65,9 @@ const setProductImage = (variantImg: string) => {
           </ul>
           <ul class="my-3 flex gap-2">
             <li
-              v-for="variant in variants"
+              v-for="(variant, index) in variants"
               :key="variant.id"
-              @mouseover="setProductImage(variant.imageUrl)"
+              @mouseover="updateVariant(index)"
               class="px-4 py-1 cursor-pointer color-circle"
               :class="variant.color === 'green' ? 'bg-green-700' : 'bg-cyan-700'"
             >
@@ -61,23 +76,16 @@ const setProductImage = (variantImg: string) => {
               </span>
             </li>
           </ul>
-
-          <p v-if="inventory > 10" class="text-green-400">In Stock</p>
-          <p v-else-if="inventory <= 10 && inventory > 0" class="text-orange-400">
-            Almost Out of Stock
-          </p>
+          <p v-if="inStock" class="text-green-400">In Stock</p>
           <p v-else class="text-red-400">Out of Stock</p>
-          <small
-            v-show="onSale && inventory > 0"
-            class="text-sm text-blue-400 font-semibold capitalize"
-          >
-            On Sale
+          <small v-show="onSale && inStock" class="text-sm text-blue-400 font-semibold capitalize">
+            {{ title }} is On Sale
           </small>
           <button
             class="button"
             @click="addToCart"
-            :disabled="!inventory"
-            :class="!inventory && 'disabled-button'"
+            :disabled="!inStock"
+            :class="!inStock && 'disabled-button'"
           >
             Add to Cart
           </button>
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
-export type Product = {
+export type TProduct = {
   id: string
   name: string
   imageUrl: string
   price: number
+}
+export type TVariant = {
+  id: number
+  color: 'blue' | 'green'
+  imageUrl: string
+  quantity: number
 }
