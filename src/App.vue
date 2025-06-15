f9c2daf54318d1610e7a72ba8ad774787ad97bea
marwen mrabti <marwen.mrabti.me@gmail.com>
2025-06-15 13:53:26 +0100

    l2: attribute binding with v-bind: or :

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import socksGreenImg from '@/assets/images/socks_green.jpeg'
 
 const product = ref('socks')
 const productDescription = ref('this socks are amazing!')
+const greenSocksImg = ref(socksGreenImg)
 </script>
 
 <template>
-  <main class="max-w-screen min-h-screen flex justify-center items-start bg-amber-100">
-    <div class="flex flex-col gap-2">
-      <h1 class="text-3xl">{{ product }}</h1>
-      <p class="text-base">
-        {{ productDescription }}
-      </p>
+  <main class="bg-amber-100 max-w-screen min-h-screen">
+    <div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <img :src="greenSocksImg" alt="green socks with vue mastery logo" />
+        </div>
+        <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p>{{ productDescription }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
